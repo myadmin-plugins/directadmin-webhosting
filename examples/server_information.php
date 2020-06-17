0ba80548e5b673e913b3219087293c8fde1af1b8
Joe Huss <detain@interserver.net>
2020-06-17 19:07:00 -0400

    updates fixing up some of cli scripts so they work again and now support passing a server name as a parameter and will load the matching db entry for the call... also added a scriptto list the admins.. and one to list the server information

--- examples/server_information.php
+++ examples/server_information.php
@@ -25,7 +25,7 @@ $sock->connect("ssl://{$server_name}", $server_port);
 //$sock->connect('http://'.$server_name, $server_port);
 $sock->set_login("admin", $password);
 
-$sock->query('/CMD_API_SHOW_ALL_USERS');
+$sock->query('/CMD_API_ADMIN_STATS');
 $result = $sock->fetch_parsed_body();
 
 print_r($result);
